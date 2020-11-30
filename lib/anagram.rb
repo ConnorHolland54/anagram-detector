b5db00db6c073f33a12868c39dcbf077eaf42f83
Connor Holland <supersoccergoalie123@gmail.com>
2020-11-30 11:37:33 -0700

    commit tree b320407ef68c87b346b90710de4b9cb2130e84b2

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(x)
+        self.word = x
+    end
+
+    def match(array)
+        anagrams = []
+        array.each do |element|
+            split_array = element.split("")
+            if split_array.sort == self.word.split("").sort
+                anagrams.push(element)
+            end
+        end
+        return anagrams
+    end
+end 
+
